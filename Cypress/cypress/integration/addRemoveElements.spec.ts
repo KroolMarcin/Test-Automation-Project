73e77c88d0df2fc892f615e98bf5a987d3d4c2ae
= <=>
2023-10-26 19:38:54 +0200

    Fix

---
+++ Cypress/cypress/integration/addRemoveElements.spec.ts
@@ -0,0 +1,18 @@
+describe('Add/Remove Elements Test', () => {
+    it('should add and remove elements', () => {
+      cy.visit('/add_remove_elements/');
+  
+      // Adding two elements
+      cy.get('[onclick="addElement()"]').click().click();
+  
+      // Checking if elements where added
+      cy.get('.added-manually').should('have.length', 2);
+  
+      // Deleting one element
+      cy.get('.added-manually').first().click();
+  
+      // Checking if elements were deleted
+      cy.get('.added-manually').should('have.length', 1);
+    });
+  });
+  
